699b423b7bd43afa1829829565de2508eebc2b97
3mimii <144124125+3mimii@users.noreply.github.com>
2024-10-21 08:01:06 -0300

    21/10 RECUPERAÇÃO

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Borboletas',
+    'quantos dias pode durar a metamorfose de largata em borboleta?',
+    'dura de dez a quinze dias'
+)
+
+criaCartao(
+    'Borboletas',
+    'quantas patas possuem as borboetas?',
+    'possuem 6 patas 3 de cada lado'
+)
+
+criaCartao(
+    'Borboletas',
+    'por que as borboletas possuem seus esqueletos do lado de fora do corpo? ',
+    'para proteger e manter a água dentro de seus corpos para que elas não sequem'
+)
+criaCartao(
+    'Borboletas',
+    'O que a borboleta significa? ',
+    'A borboleta é considerada o símbolo da transformação, da felicidade, da beleza, da inconstância, da eferemidade da natureza e da renovação.'
+)
+
